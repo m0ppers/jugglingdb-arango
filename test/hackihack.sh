84c37a976f6b1fd8aab2215023cda3e2e04d0675
Andreas Streichardt <andreas.streichardt@gmail.com>
2013-03-25 21:14:47 +0100

    :O HACKIHACK :O

---
+++ test/hackihack.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# hackihack until jugglingdb tests are more generic :O
+sed -e "111s/mongodb/arango/g" node_modules/jugglingdb/test/common_test.js | sed -e "1377s/mongodb/arango/g" | sed -e "1386s/mongodb/arango/g" | sed -e "1392s/mongodb/arango/g" > node_modules/jugglingdb/test/common_test.js_new && cp node_modules/jugglingdb/test/common_test.js_new node_modules/jugglingdb/test/common_test.js
