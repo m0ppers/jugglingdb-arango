494fc55c8c1ccc631f4dfa3e38ceefbdf87dfe68
Andreas Streichardt <andreas.streichardt@gmail.com>
2014-03-05 13:01:38 +0100

    Allow different databse for testing

--- test/init.js
+++ test/init.js
@@ -3,7 +3,7 @@ module.exports = require('should');
 var Schema = require('jugglingdb').Schema;
 
 global.getSchema = function() {
-    var db = new Schema(__dirname + '/..');
+    var db = new Schema(__dirname + '/..', {url: "http://localhost:8529/" + (process.env.TESTDATABASE || "") });
     db.name = 'arango';
     return db;
 };
